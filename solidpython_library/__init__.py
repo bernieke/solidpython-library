51f862c920d5f24fba978e33f69d95a91435ee1a
Bernard Kerckenaere <bernieke@bernieke.com>
2025-11-14 13:56:23 +0100

    add extrusion width

--- solidpython_library/__init__.py
+++ solidpython_library/__init__.py
@@ -1,4 +1,4 @@
-from .common import C, SWELL
+from .common import C, SWELL, EXTRUSION_WIDTH
 from .heatset_insert import HEATSET_INSERT_CONFIG, heatset_insert
 from .screwhole import SCREW_CONFIG, screwhole
 from .fillet import fillet
@@ -7,6 +7,7 @@ from .rounded_cube import rounded_cube
 __all__ = [
     C,
     SWELL,
+    EXTRUSION_WIDTH,
     HEATSET_INSERT_CONFIG, heatset_insert,
     SCREW_CONFIG, screwhole,
     fillet,
--- solidpython_library/common.py
+++ solidpython_library/common.py
@@ -1,2 +1,3 @@
 C = 0.01  # OpenSCAD intersection compensation
 SWELL = 0.5  # 0.25mm per side, only in X and Y
+EXTRUSION_WIDTH = 0.45
